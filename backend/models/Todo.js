bf3af4675a406ca0d2159f29dca6ea99acb280f1
kumarvermarounak2024 <kumarvermarounak2024@gmail.com>
2025-07-12 11:43:24 +0530

    4th commit

--- backend/models/Todo.js
+++ backend/models/Todo.js
@@ -1,9 +1,9 @@
-import mongoose from 'mongoose';
+const mongoose = require('mongoose');
 
 const todoSchema = new mongoose.Schema({
   text: { type: String, required: true },
   completed: { type: Boolean, default: false },
 });
 
-export default mongoose.model('Todo', todoSchema);
+module.exports = mongoose.model('Todo', todoSchema);
 
